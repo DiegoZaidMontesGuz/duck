60c232c3b4880162967377fed8309e8a0cbc992b
DiegoMontesKid <154762565+DiegoMontesKid@users.noreply.github.com>
2025-09-03 22:08:39 -0700

    quack

---
+++ duckGame/progress_bar.gd
@@ -0,0 +1,23 @@
+extends Control
+
+@onready var bar: TextureProgressBar = $Bar
+var follow := true
+var offset := Vector2(12, 16)  # shift so it doesn't sit directly on the cursor
+
+func _ready() -> void:
+	bar.min_value = 0
+	bar.max_value = 1000
+	bar.value = 1000
+
+func _process(_delta: float) -> void:
+	if not follow:
+		return
+
+	# Mouse in viewport (screen) coordinates
+	var mouse_screen: Vector2 = get_viewport().get_mouse_position()
+
+	# Convert to this Control's local space (important if this Control is nested/scaled)
+	bar.position = bar.get_parent().get_local_mouse_position()
+	
+	bar.position.x -= 20
+	bar.position.y -= 20
